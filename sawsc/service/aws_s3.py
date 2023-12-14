fb748437d10fe462216b99302d3bbde78fb7e4d3
sambler <Develop@Shaneware.Biz>
2023-12-14 17:05:03 +1030

    list s3 buckets

--- sawsc/service/aws_s3.py
+++ sawsc/service/aws_s3.py
@@ -1,6 +1,8 @@
 
+import boto3
 import tkinter as tk
 import ttkbootstrap as ttk
+from ttkbootstrap.tooltip import ToolTip
 
 from . import ListBase
 
@@ -8,9 +10,20 @@ Opts = None # set from gui when making ListFrame
 
 name = 'S3'
 
+s3 = boto3.client('s3')
+
 class ListFrame(ListBase):
     def thr_get_data(self):
+        bucket_list = s3.list_buckets()
         self.clear_list()
-        l = ttk.Label(self, text=f' {name} data here')
-        l.grid(row=0, column=0)
-
+        for b in bucket_list['Buckets']:
+            item = ttk.Frame(self, borderwidth=2, relief=tk.RIDGE)
+            item.pack(side=tk.TOP, expand=True, fill=tk.X)
+            l = ttk.Button(item, text=b['CreationDate'], bootstyle='link',
+                            command=lambda tx=b['CreationDate']: self.copy_to_clip(tx))
+            l.grid(row=0, column=1, sticky=tk.W, padx=PADDING)
+            tt = ToolTip(l, text='Created')
+            l = ttk.Button(item, text=b['Name'], bootstyle='link',
+                            command=lambda tx=b['Name']: self.copy_to_clip(tx))
+            l.grid(row=0, column=2, sticky=tk.W, padx=PADDING)
+            tt = ToolTip(l, text='Bucket name')
