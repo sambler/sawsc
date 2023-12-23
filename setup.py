304a6f68b9b1f5925db40b2629e27d1547e41a8e
sambler <Develop@Shaneware.Biz>
2023-12-23 17:25:56 +1030

    bump version after bugfix

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ URL = 'https://github.com/sambler/sawsc'
 EMAIL = 'develop@shaneware.biz'
 AUTHOR = 'Shane Ambler'
 REQUIRES_PYTHON = '>=3.8.0'
-VERSION = '1.0.1'
+VERSION = '1.0.2'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
